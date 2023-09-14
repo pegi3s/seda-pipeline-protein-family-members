9312a8c3223cee208e751653ddea98cd8ea8d212
Hugo López-Fernández <hlfernandez@uvigo.es>
2023-09-14 18:13:49 +0200

    Fixes extract-headers script

--- task-scripts/extract-headers.sh
+++ task-scripts/extract-headers.sh
@@ -2,8 +2,16 @@
 set -o nounset
 set -o errexit
 
+TASK_AFTER=$(env | grep "^task_after=" | sed 's/^task_after*=//')
+
+echo ${task_id}
+echo ${TASK_AFTER}
+
 INPUT=${workingDirectory}/${output}/${TASK_AFTER}
 OUTPUT=${workingDirectory}/${output}/${task_id}
+
+mkdir -p ${OUTPUT}
+
 FILES=$(ls -1 ${INPUT})
 
 output_file="${OUTPUT}/stats.tsv"
--- task-scripts/final-task.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "This is the final task, which is a bash script instead of a SEDA command."
-echo "This kind of tasks may be useful for performing non-SEDA operations."
